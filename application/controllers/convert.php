74e524869111e85b79ecc592697b980a01e93a23
Hans Bentlöv <hanserikb@gmail.com>
2013-08-14 21:39:25 +0200

    Refactoring

--- application/controllers/convert.php
+++ application/controllers/convert.php
@@ -3,6 +3,7 @@
 class Convert extends CI_Controller {
 
     private $api_key = "224cd8f6d9f9c8fb23bfc5a4432f8a91782b87f9";
+    private $inputData;
     /*
         Currency API
 
@@ -20,30 +21,44 @@ class Convert extends CI_Controller {
     public function __construct()
     {
         parent::__construct();
+        $this->load->helper('url');
     }
 
     public function index()
     {
-        $this->load->helper('url');
         $this->load->view('convert/convert');
     }
 
-    public function convert()
+    public function doConvert()
     {
         // Fetch input variables
-        $from = $this->input->post('from', TRUE);
-        $to = $this->input->post('to', TRUE);
-        $amount = $this->input->post('amount', TRUE);
+        $this->getInput();
 
-        // Build URL
-        $url = "http://currency-api.appspot.com/api/{$from}/{$to}.json?key={$this->api_key}&amount={$amount}";
-
-        // Make API call and parse it
-        $result = file_get_contents($url);
+        // Make the request
+        $result = $this->makeRequest();
 
         // Set response content type to json and send the data
         $this->output->set_content_type('application/json')->set_output($result);
 
+    }
+
+    private function getInput()
+    {
+        // Fetch input data
+        $this->inputData = [
+            "from" => $this->input->post('from', TRUE),
+            "to" => $this->input->post('to', TRUE),
+            "amount" => $this->input->post('amount', TRUE)
+        ];
+    }
+
+    private function makeRequest()
+    {
+        // Build URL
+        $url = "http://currency-api.appspot.com/api/{$this->inputData['from']}/{$this->inputData['to']}.json?key={$this->api_key}&amount={$this->inputData['amount']}";
+
+        // Make API call and parse it
+        return file_get_contents($url);
     }
 }
 
